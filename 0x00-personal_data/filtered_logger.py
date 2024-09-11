1d95a230c0cbafcffe14738d5db9fe1b85a5d2e2
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-09-11 10:44:07 +0000

    Regex-ing

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Module with a function called filter_datum"""
+import logging
+import re
+
+
+def filter_datum(fields, redaction, message, separator):
+    """
+    Obfuscates the fields in the log message.
+
+    Args:
+        fields (List[str]): Fields to obfuscate.
+        redaction (str): The string to replace the field values.
+        message (str): The log message.
+        separator (str): The separator used in the log message.
+
+    Returns:
+        str: The obfuscated log message.
+    """
+    for field in fields:
+	message = re.sub(f"{field}=(.*?){separator}", f"{field}={redaction}{separator}", message)
+    return message
