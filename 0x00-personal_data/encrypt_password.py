9dfcebe35116bfb7b64c45e4b590c596aa239fc3
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-09-12 12:21:20 +0300

    Debugging

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -2,19 +2,15 @@
 """ Returns a salted, hashed password, byte in string """
 import bcrypt
 
+
 def hash_password(password: str) -> bytes:
     """
     Hashes a password using bcrypt with a salt.
-
     Args:
         password (str): The plaintext password to hash.
-
     Returns:
         bytes: The salted and hashed password.
     """
     password_bytes = password.encode('utf-8')
-    
     salt = bcrypt.gensalt()
-    
     return bcrypt.hashpw(password_bytes, salt)
-
