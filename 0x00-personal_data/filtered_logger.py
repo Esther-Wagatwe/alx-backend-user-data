ae08fa0121f0f11af410492ff9a3ad845d711629
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-09-12 10:35:00 +0300

    Debugging

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,9 +2,11 @@
 """Module with a function called filter_datum"""
 import logging
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """
     Obfuscates the fields in the log message.
 
