84028c52cdb3e7edee48ee3df7e541089ba6412b
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-09-12 11:16:19 +0300

    Debugging

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -53,7 +53,6 @@ def get_logger() -> logging.Logger:
     logger.setLevel(logging.INFO)
     logger.propagate = False
 
-
     stream_handler = logging.StreamHandler
 
     formatter = RedactingFormatter(PII_FIELDS)
