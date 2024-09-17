1c7abea1b4a1937dc530224b1ded28f2c6236deb
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-09-17 09:42:52 +0300

    Debugging

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -63,14 +63,11 @@ class BasicAuth(Auth):
         """
         Retrieve the User instance based on email and password.
         """
-        if user_email is None or not isinstance(user_email, str):
-            return None
-
-        if user_pwd is None or not isinstance(user_pwd, str):
+        if not isinstance(user_email, str) or not isinstance(user_pwd, str):
             return None
 
         users = User.search({"email": user_email})
-        if not users or len(users) == 0:
+        if not users:
             return None
 
         user = users[0]
