7b2d0d0c16d299b72f64432aed7ed06ef84df930
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-09-18 12:19:36 +0300

    Et moi et moi et moi!

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""Authentication module."""
+from flask import request
+from typing import List, TypeVar
+import fnmatch
+
+
+class Auth:
+    """Basic authentication class"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Method to determine if authentication is required for a given path.
+        """
+        if path is None:
+            return True
+
+        if excluded_paths is None or not excluded_paths:
+            return True
+
+        if not path.endswith('/'):
+            path += '/'
+
+        normalized_excluded_paths = [p if p.endswith('/') else p + '/'
+                                     for p in excluded_paths]
+
+        for excluded_path in normalized_excluded_paths:
+            if fnmatch.fnmatch(path, excluded_path):
+                return False
+
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Method to retrieve the Authorization header from the request.
+        """
+        if request is None:
+            return None
+
+        if 'Authorization' not in request.headers:
+            return None
+
+        return request.headers.get('Authorization')
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Method to retrieve the current user based on the request.
+        """
+        return None
