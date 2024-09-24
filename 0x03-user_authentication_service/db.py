f5b5d531fd2be2b9f03abc1ac325fd0d96a9e006
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-09-24 10:55:15 +0300

    Debugging

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -34,8 +34,12 @@ class DB:
         """
         Adds a new user to the db with the given email and hashed password.
         """
-        new_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(new_user)
-        self._session.commit()
+        try:
+            new_user = User(email=email, hashed_password=hashed_password)
+            self._session.add(new_user)
+            self._session.commit()
 
+        except Exception:
+            self._session.rollback()
+            new_user = None
         return new_user
