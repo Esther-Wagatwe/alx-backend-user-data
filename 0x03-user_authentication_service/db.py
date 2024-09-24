71c89eb85d2ec0f2e722ad594eae61b0eed417a2
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-09-24 11:48:33 +0300

    Update user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -58,3 +58,15 @@ class DB:
             raise NoResultFound()
         except InvalidRequestError:
             raise InvalidRequestError()
+
+    def update_user(self, user_id: int, **kwargs: Dict[str, str]) -> None:
+        """
+        Updates a user's attributes in the database.
+        """
+        user = self.find_user_by(id=user_id)
+
+        for key, value in kwargs.items():
+            if not hasattr(User, key):
+                raise ValueError
+            setattr(user, key, value)
+        self._session.commit()
