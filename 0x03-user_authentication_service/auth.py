0c376ae214587a3f88f286286a17fb2022e1d76e
Esther-Wagatwe <estherwagatwe@gmail.com>
2024-09-24 13:55:10 +0300

    Find user by session ID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -64,6 +64,7 @@ class Auth:
         if session_id:
             try:
                 user = self._db.find_user_by(session_id=session_id)
+                return user
             except NoResultFound:
                 return None
         return None
